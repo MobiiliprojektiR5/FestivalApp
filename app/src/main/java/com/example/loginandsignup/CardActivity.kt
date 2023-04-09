3e2f37c86e90f13c0bb697ed6f2f93919836c0b9
Miia <t1jami00@students.oamk.fi>
2023-04-09 12:00:58 +0300

    firebase-hommia, ei vielä toimi

--- app/src/main/java/com/example/loginandsignup/CardActivity.kt
+++ app/src/main/java/com/example/loginandsignup/CardActivity.kt
@@ -2,11 +2,28 @@ package com.example.loginandsignup
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import com.example.loginandsignup.databinding.ActivityCardBinding
 
 
 class CardActivity : AppCompatActivity() {
+
+
+    private lateinit var binding : ActivityCardBinding
+    private lateinit var database : DataBaseReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_card)
+        binding = ActivityCardBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.readdataBtn.setOnClickListener {
+            val name : String = binding
+        }
     }
+
+    private fun readData(name:String){
+        database = FirebaseDatabase.getInstance().getReference( path:"0")
+    }
+
 }
